cfccf8eec4ef7985fe45eeceb5d7314ef4c9af69
Thiago Meireles <thiago22md@gmail.com>
2024-02-12 11:58:43 -0300

    teste_front-end

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,6 +1,26 @@
+import { calculaNovoSaldo } from './index';
 
+describe('Quando realizo uma transação', () => {
+  test('Que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50
+    }
+    const novoSaldo = calculaNovoSaldo(transacao, 100)
+    expect(novoSaldo).toBe(150);
+  });
 
-test('Deve rewtornar o valor do saldo atualizado com o rendimento', () => {
+  test('Que é uma Transferência, o saldo deve diminuir', () => {
+    const transacao = {
+      transacao: 'Transferência',
+      valor: 50
+    }
+    const novoSaldo = calculaNovoSaldo(transacao, 100)
+    expect(novoSaldo).toBe(50);
+  });
+});
+
+test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
   const calculaRendimento = jest.fn(saldo => saldo + saldo*0.005);
   const saldo = 100;
   const novoSaldo = calculaRendimento(saldo);
